81502cb3bd959d4274ff165b11ec1ee5c4596ac4
Nguyễn Tín <tin000000000z@gmail>
2023-12-08 18:22:35 +0700

    ss4-bt1

--- module2/src/ss4_Class_and_Object_in_java/BaiTap1_Build_the_Quadratic_Equation_class.java
+++ module2/src/ss4_Class_and_Object_in_java/BaiTap1_Build_the_Quadratic_Equation_class.java
@@ -1,12 +1,33 @@
 package ss4_Class_and_Object_in_java;
 
+import java.util.Scanner;
+
 public class BaiTap1_Build_the_Quadratic_Equation_class {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap a : ");
+        double A = scanner.nextDouble();
+        System.out.println("nhap b : ");
+        double B = scanner.nextDouble();
+        System.out.println("nhap c : ");
+        double C = scanner.nextDouble();
 
-    }
+        Quadratic quadratic = new Quadratic(A,B,C);
+        double delta = quadratic.getDiscriminamt();
+        System.out.println("Delta = "+delta);
+
+        if (delta<0){
+            System.out.println("phuong trinh vo nghiem");
+    }else {
+            if (delta ==0){
+                System.out.println("phuong trinh co nghiem kep : " +quadratic.getRoot1());
+            }else {
+                System.out.println("phuong trinh co 2 nghiem la "+quadratic.getRoot1()+" va "+quadratic.getRoot2());
+            }
+        }
 }
 
- class Quadratic {
+ static class Quadratic {
     double a,b,c;
 
      public Quadratic(double a, double b, double c) {
@@ -37,6 +58,25 @@ public class BaiTap1_Build_the_Quadratic_Equation_class {
 
      public void setC(double c) {
          this.c = c;
+     }
+
+     double getDiscriminamt(){
+         return (Math.pow(b,2)-4*a*c);
+     }
+
+     double getRoot1(){
+         if (getDiscriminamt()>=0){
+             return (-b+Math.sqrt(getDiscriminamt()))/2/a;
+         }else
+             return 0;
+     }
+
+     double getRoot2(){
+         if (getDiscriminamt()>=0){
+             return (-b-Math.sqrt(getDiscriminamt()))/2/a;
+         }else
+             return 0;
+     }
      }
  }
 
