97f4ed05150120628f019099291be0c28bccbe79
Nguyễn Tín <tin000000000z@gmail>
2023-12-08 17:59:55 +0700

    up

---
+++ module2/src/ss4_Class_and_Object_in_java/BaiTap1_Build_the_Quadratic_Equation_class.java
@@ -0,0 +1,42 @@
+package ss4_Class_and_Object_in_java;
+
+public class BaiTap1_Build_the_Quadratic_Equation_class {
+    public static void main(String[] args) {
+
+    }
+}
+
+ class Quadratic {
+    double a,b,c;
+
+     public Quadratic(double a, double b, double c) {
+         this.a = a;
+         this.b = b;
+         this.c = c;
+     }
+
+     public double getA() {
+         return a;
+     }
+
+     public void setA(double a) {
+         this.a = a;
+     }
+
+     public double getB() {
+         return b;
+     }
+
+     public void setB(double b) {
+         this.b = b;
+     }
+
+     public double getC() {
+         return c;
+     }
+
+     public void setC(double c) {
+         this.c = c;
+     }
+ }
+
